05767f2ea4d0deb59231ea9423c7bf23a2bf2be4
snehamol <snehamolkaniyeri@gmail.com>
2023-12-12 11:01:25 +0530

    last commit

--- client/src/TrainerDashboard/partials/dashboard/WelcomeBanner.jsx
+++ client/src/TrainerDashboard/partials/dashboard/WelcomeBanner.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
 
 function WelcomeBanner() {
+  const [email, setEmail] = useState('');
+  useEffect(() => {
+    const storedEmail = localStorage.getItem('email');
+    if (storedEmail) {
+      setEmail(storedEmail);
+    }
+  }, []);
   return (
     <div className="relative bg-indigo-200 dark:bg-indigo-500 p-4 sm:p-6 rounded-sm overflow-hidden mb-8">
       {/* Background illustration */}
@@ -47,7 +55,7 @@ function WelcomeBanner() {
 
       {/* Content */}
       <div className="relative">
-        <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">Good afternoon, Acme Inc. 👋</h1>
+        <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">Good afternoon,  {email} 👋</h1>
         <p className="dark:text-indigo-200">Here is what’s happening with your projects today:</p>
       </div>
     </div>
